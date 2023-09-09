b85160d3ee9c463ead0bd9aea3c56a801b52a698
Alok Gandhi <alok.gandhi2002@gmail.com>
2023-09-09 20:29:29 +0200

    Added transform handling to hittables.

--- src/pyraytracer/hittable.py
+++ src/pyraytracer/hittable.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod, abstractproperty
-from typing import Optional
+from typing import Optional, Tuple
 
 from pydantic import BaseModel
 
 from .material import Material
+from .transformation import Transformation
 from .vec3 import Vec3
 
 
@@ -22,16 +23,99 @@ class Hittable(ABC):
     def material(self) -> Material:
         raise NotImplementedError
 
+    @abstractproperty
+    def scale(self) -> Vec3:
+        raise NotImplementedError
+
+    @abstractproperty
+    def transform(self) -> Transformation:
+        raise NotImplementedError
+
+    @abstractproperty
+    def rotation(self) -> Vec3:
+        raise NotImplementedError
+
     def get_hit_data(self, ray: Vec3, ray_origin: Vec3) -> HitData:
-        t: Optional[float] = self.get_t(ray=ray, ray_origin=ray_origin)
+        ray_t, ray_origin_t = self._ray_global_to_local(
+            ray=ray,
+            ray_origin=ray_origin,
+        )
+
+        t: Optional[float] = self.get_t(ray=ray_t, ray_origin=ray_origin_t)
 
         point: Optional[Vec3] = None
         normal: Optional[Vec3] = None
         if t is not None:
-            point = ray_origin + (ray * t)
-            normal = self.get_normal(hit_point=point)
+            point, normal = self._point_normal_local_to_global(
+                t=t,
+                ray=ray_t,
+                ray_origin=ray_origin_t,
+            )
 
         return HitData(t=t, point=point, normal=normal)
+
+    def _ray_global_to_local(
+            self,
+            ray: Vec3,
+            ray_origin: Vec3
+    ) -> Tuple[Vec3, Vec3]:
+
+        hittable_transform: Transformation = self.transform
+
+        if hittable_transform.is_default:
+            # No need to transform ray if hittable has no transform set
+            # saves a lot of compute for matrix operations.
+            return ray, ray_origin
+
+        # First tranform ray origin to local space
+        ray_origin_t = hittable_transform.global_to_local(point=ray_origin)
+
+        # Next get the point for ray direction in global space
+        ray_tip_global = ray_origin + ray
+
+        # Now tranform this global ray tip to local
+        ray_tip_local = hittable_transform.global_to_local(point=ray_tip_global)
+
+        # Now get back the ray direction in local
+        ray_t = ray_tip_local - ray_origin_t
+
+        # Normalize ray
+        ray_t = Vec3.normalize(v=ray_t)
+
+        return ray_t, ray_origin_t
+
+    def _point_normal_local_to_global(
+            self, t: float,
+            ray: Vec3,
+            ray_origin: Vec3
+    ) -> Tuple[Vec3, Optional[Vec3]]:
+
+        hittable_transform: Transformation = self.transform
+
+        local_point = ray_origin + (ray * t)
+
+        if self.material.is_constant:
+            # We do not need normal calculation for constant materials
+            local_normal = None
+        else:
+            local_normal = self.get_normal(hit_point=local_point)
+
+        if hittable_transform.is_default:
+            # No need to transform ray if hittable has no transform set
+            # saves a lot of compute for matrix operations.
+            return local_point, local_normal
+
+        point = hittable_transform.local_to_global(point=local_point)
+
+        if local_normal is not None:
+            local_normal_tip = local_point + local_normal
+            global_normal_tip = hittable_transform.local_to_global(point=local_normal_tip)
+            normal = global_normal_tip - point
+            normal = Vec3.normalize(v=normal)
+        else:
+            normal = None
+
+        return point, normal
 
     @abstractmethod
     def get_t(self, ray: Vec3, ray_origin: Vec3) -> Optional[float]:
